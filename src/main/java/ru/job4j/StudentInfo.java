9cf9c6fe66b30e2fce76f49d17b4596370c72893
migov <miha85@gmail.com>
2022-02-02 21:35:07 +0700

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Petr Arsentev");
         System.out.println("01/02/1989");
     }
