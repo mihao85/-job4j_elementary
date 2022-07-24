a28b1c04ec429f106a3b6beea1982b4ff7d2a4a0
mihao85 <mihao85@gmail.com>
2022-07-24 14:52:30 +0700

    1. Модульные тесты. [#159435]

--- src/main/java/ru/job4j/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/ru/job4j/converter/Converter.java
@@ -2,12 +2,12 @@ package ru.job4j.ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
+        float rsl = value / 1;
         return rsl;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
+        float rsl = value / 20;
         return rsl;
     }
 
