16c9eff8a5764d6ff387283175b08b725574a36f
mihao85 <mihao85@gmail.com>
2022-08-23 00:07:45 +0700

    1.2. Входные данные в тесте [#297750 #325393]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -1,15 +1,16 @@
-package ru.job4j;
+package ru.job4j.condition;
 
 public class X2 {
     public static int calc(int a, int b, int c, int x) {
-        return a * x * x + b * x + c;
+       int rsll = a * x * x + b * x + c;
+        return rsll;
     }
 
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
         int c = 1;
-        int x = 1;
+        int x = 0;
         int rsl = X2.calc(a, b, c, x);
         System.out.println(rsl);
     }
