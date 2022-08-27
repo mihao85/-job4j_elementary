443f3575e624e23033777a2bf89f6781b510427d
mihao85 <mihao85@gmail.com>
2022-08-27 17:01:13 +0700

    4. Тесты для стороны прямоугольника.

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,37 +1,37 @@
 package ru.job4j.condition;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.offset;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class SqAreaTest {
+public class SqAreaTest {
+
     @Test
-    void whenP2K4Square1() {
-        double expected = 0.16;
-        int p = 2;
-        double k = 4;
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    void whenP4K4Square1() {
-        double expected = 0.64;
-        int p = 4;
-        double k = 4;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    void whenP8K4Square1() {
-        double expected = 2.56;
+    public void whenP8K2Square2() {
+        double expected = 3.55;
         int p = 8;
-        double k = 4;
+        double k = 2;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void whenP20K2Square2() {
+        double expected = 22.22;
+        int p = 20;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
 }
