61391ae17176483bf31da0aae3ad296ff550d60b
mihao85 <mihao85@gmail.com>
2022-08-28 09:10:20 +0700

    1. Модульные тесты. [#159435 #325302]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,6 +1,4 @@
-
 package ru.job4j.converter;
-
 
 public class Converter {
     public static float rubleToEuro(float value) {
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.converter;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 
